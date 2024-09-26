9059b36fd471e47317c100d9faaa0a85e9d6647f
Amaan Ali Khan <amaanali.amaanali@gmail.com>
2024-09-26 15:59:32 +0530

    Added Response Entity in controller

--- src/main/java/com/aak/journalApp/entity/JournalEntry.java
+++ src/main/java/com/aak/journalApp/entity/JournalEntry.java
@@ -2,21 +2,15 @@ package com.aak.journalApp.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-<<<<<<< HEAD
-
-@Getter
-@Setter
-public class JournalEntry {
-    private long id;
-    private String content;
-    private String title;
-=======
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
-import java.util.Date;
+
+@Getter
+@Setter
+public class JournalEntry {
 
 @Getter
 @Setter
@@ -27,7 +21,6 @@ public class JournalEntry {
     private String content;
     private String title;
     private LocalDateTime date;
->>>>>>> 7daba04 (Initial commit on new branch)
 
-
+}
 }
