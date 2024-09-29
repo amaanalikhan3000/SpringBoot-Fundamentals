e142895436a1b3ab2b2b4615b5665f77df477a18
Amaan Ali Khan <amaanali.amaanali@gmail.com>
2024-09-30 02:02:31 +0530

    Initial commit for transaction Management and Annotation

--- src/main/java/com/aak/journalApp/JournalApplication.java
+++ src/main/java/com/aak/journalApp/JournalApplication.java
@@ -2,7 +2,13 @@ package com.aak.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 public class JournalApplication {
 
@@ -10,4 +16,9 @@ public class JournalApplication {
 		SpringApplication.run(JournalApplication.class, args);
 	}
 
+	@Bean
+	public PlatformTransactionManager add(MongoDatabaseFactory dbFactory){
+		return new MongoTransactionManager(dbFactory);
+	}
 }
+
